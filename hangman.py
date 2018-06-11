476813df579847248f61171add05cbb76b5775e3
Ju111 <SchneiderJu@gmx.net>
2018-06-11 17:47:11 +0200

    add single letters of word to guess to a list

--- hangman.py
+++ hangman.py
@@ -1,11 +1,16 @@
-words = ['sunflower', 'tulip', 'daisy']
+# words = ['sunflower', 'tulip', 'daisy']
 
 # def wrong_guess_count(word, guesses):
+
+def show_guess(word, guesses):
+    show = list(word)
+
 
 def make_guess(word, guesses):
     guess = input('Guess a letter ')
     print ('Player wrote: %s' % (guess))
     guesses.append(guess)
-    print(guesses)
+    show_guess(word, guesses)
+
 
 make_guess('sunflower', [])
