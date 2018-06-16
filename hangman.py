6299f503fda23c3f79e4afc324892f891e31fd74
Ju111 <SchneiderJu@gmx.net>
2018-06-16 15:11:52 +0200

    count the number of wrong guesses

--- hangman.py
+++ hangman.py
@@ -1,6 +1,8 @@
 # words = ['sunflower', 'tulip', 'daisy']
 
-# def wrong_guess_count(word, guesses):
+def wrong_guess_count(word, guesses):
+    wrong_guesses = [list(x) for x in guesses if x not in word]
+    return len(wrong_guesses)
 
 
 def show_guess(word, guesses):
@@ -12,12 +14,12 @@ def show_guess(word, guesses):
             print ('_', end=" ")
 
 
-
 def make_guess(word, guesses):
     guess = input('Guess a letter ')
     print ('Player wrote: %s' % (guess))
     guesses.append(guess)
     show_guess(word, guesses)
+    wrong_guess_count(word, guesses)
 
 
 make_guess('sunflower', [])
