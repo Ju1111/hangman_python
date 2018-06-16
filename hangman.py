3d08ddb59a5c5b222c1064a1aa1a9f91e43f267d
Ju111 <SchneiderJu@gmx.net>
2018-06-16 15:44:15 +0200

    game is lost after more than 6 wrong guesses

--- hangman.py
+++ hangman.py
@@ -5,6 +5,11 @@ def wrong_guess_count(word, guesses):
     return len(wrong_guesses)
 
 
+def is_winner(word, guesses):
+    if wrong_guess_count(word, guesses) > 6:
+        print ('You Lost!')
+
+
 def show_guess(word, guesses):
     show = list(word)
     for x in show:
@@ -15,11 +20,11 @@ def show_guess(word, guesses):
 
 
 def make_guess(word, guesses):
+    is_winner(word, guesses)
     guess = input('Guess a letter ')
     print ('Player wrote: %s' % (guess))
     guesses.append(guess)
     show_guess(word, guesses)
-    wrong_guess_count(word, guesses)
 
 
 make_guess('sunflower', [])
